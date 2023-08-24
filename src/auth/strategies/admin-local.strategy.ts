2a8728cda945c28e8406d2a226e39bc274d3ad11
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2023-08-24 21:33:12 +0700

    fix: fix typo

--- src/auth/strategies/admin-local.strategy.ts
+++ src/auth/strategies/admin-local.strategy.ts
@@ -1,13 +1,12 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthService } from '../auth.service';
-import { ErrorResponseDto } from 'src/common/dto/response.dto';
 
 @Injectable()
 export class AdminLocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super({ userNameField: 'email' });
+    super({ usernameField: 'email' });
   }
 
   async validate(email: string, password: string) {

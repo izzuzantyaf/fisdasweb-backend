3919dd54c35fcc4b8107de6351e58dd037d94134
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2024-12-22 00:38:40 +0700

    chore: deactivate mongo module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
-import { MongoModule } from 'src/common/database/mongodb/mongo.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Admin from 'src/admin/entities';
 import AdminPostgresRepository from 'src/admin/repo';
 
 @Module({
-  imports: [MongoModule, TypeOrmModule.forFeature([Admin])],
+  imports: [TypeOrmModule.forFeature([Admin])],
   controllers: [AdminController],
   providers: [AdminService, AdminPostgresRepository],
   exports: [AdminService, AdminPostgresRepository],
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from 'src/auth/auth.module';
-import { HandoutModule } from 'src/handout/handout.module';
+// import { HandoutModule } from 'src/handout/handout.module';
 import { CodeOfConductModule } from 'src/code-of-conduct/code-of-conduct.module';
 import { OrganigramModule } from 'src/organigram/organigram.module';
-import { ScheduleModule } from 'src/schedule/schedule.module';
-import { AssistantModule } from 'src/assistant/assistant.module';
-import { LabModuleModule } from 'src/lab-module/lab-module.module';
-import { ArticleModule } from 'src/article/article.module';
-import { OpenAIModule } from './common/openai/openai.module';
-import { UnsplashModule } from './common/unsplash/unsplash.module';
+// import { ScheduleModule } from 'src/schedule/schedule.module';
+// import { AssistantModule } from 'src/assistant/assistant.module';
+// import { LabModuleModule } from 'src/lab-module/lab-module.module';
+// import { ArticleModule } from 'src/article/article.module';
+// import { OpenAIModule } from './common/openai/openai.module';
+// import { UnsplashModule } from './common/unsplash/unsplash.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmModuleOptions } from './common/database/typeorm/data-source-config';
-import { AwsSesModule } from './common/aws-ses/aws-ses.module';
+// import { AwsSesModule } from './common/aws-ses/aws-ses.module';
 
 @Module({
   imports: [
@@ -23,16 +23,16 @@ import { AwsSesModule } from './common/aws-ses/aws-ses.module';
     TypeOrmModule.forRoot(typeOrmModuleOptions),
     AdminModule,
     AuthModule,
-    HandoutModule,
+    // HandoutModule,
     CodeOfConductModule,
     OrganigramModule,
-    ScheduleModule,
-    AssistantModule,
-    LabModuleModule,
-    ArticleModule,
-    OpenAIModule,
-    UnsplashModule,
-    AwsSesModule,
+    // ScheduleModule,
+    // AssistantModule,
+    // LabModuleModule,
+    // ArticleModule,
+    // OpenAIModule,
+    // UnsplashModule,
+    // AwsSesModule,
   ],
 })
 export class AppModule {}

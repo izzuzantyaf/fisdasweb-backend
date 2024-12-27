e1178bb814ed9edba2ee3847dea05f51fc5ed1b2
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2024-12-27 21:58:05 +0700

    fix: add query idle timeout

--- src/common/database/typeorm/data-source-config.ts
+++ src/common/database/typeorm/data-source-config.ts
@@ -1,6 +1,7 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { config } from 'dotenv';
 import { SeederOptions } from 'typeorm-extension';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 config();
 
@@ -13,9 +14,13 @@ export const typeOrmDataSourceOptions: DataSourceOptions & SeederOptions = {
   seeds: ['dist/common/database/seeders/**/*{.ts,.js}'],
 };
 
-export const typeOrmModuleOptions = {
+export const typeOrmModuleOptions: TypeOrmModuleOptions = {
   ...typeOrmDataSourceOptions,
   autoLoadEntities: true,
+  applicationName: 'fisdasweb',
+  extra: {
+    idleTimeoutMillis: 30_000,
+  },
 };
 
 const typeOrmDataSource = new DataSource({

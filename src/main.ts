c45491e78263812032292260a066958866634fcc
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2024-12-31 22:41:26 +0700

    fix: fix cors

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,7 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    // TODO: don't allow cors in production, only allow the fisdasweb and fisdascms
-    cors: {
-      origin: [process.env.FISDASWEB_ADMIN_URL || ''],
-      credentials: true,
-    }, // jika cors true maka semua origin bisa mengakses API
+    cors: true, // jika cors true maka semua origin bisa mengakses API
     logger: process.env.APP_ENV === 'production' ? ['log'] : ['verbose'],
   });
 
